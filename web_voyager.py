63689f861eb8a2a35443197b9f1577f32ceb766a
zedwei <ran.wei@ymail.com>
2024-12-03 14:25:22 -0800

    init commit

---
+++ web_voyager.py
@@ -0,0 +1,365 @@
+# %%
+import os
+from getpass import getpass
+
+
+def _getpass(env_var: str):
+    if not os.environ.get(env_var):
+        os.environ[env_var] = getpass(f"{env_var}=")
+
+_getpass("OPENAI_API_KEY")
+
+# %%
+import nest_asyncio
+
+# This is just required for running async playwright in a Jupyter notebook
+nest_asyncio.apply()
+
+# %%
+from typing import List, Optional
+from typing_extensions import TypedDict
+
+from langchain_core.messages import BaseMessage, SystemMessage
+from playwright.async_api import Page
+
+
+class BBox(TypedDict):
+    x: float
+    y: float
+    text: str
+    type: str
+    ariaLabel: str
+
+
+class Prediction(TypedDict):
+    action: str
+    args: Optional[List[str]]
+
+
+# This represents the state of the agent
+# as it proceeds through execution
+class AgentState(TypedDict):
+    page: Page  # The Playwright web page lets us interact with the web environment
+    input: str  # User request
+    img: str  # b64 encoded screenshot
+    bboxes: List[BBox]  # The bounding boxes from the browser annotation function
+    prediction: Prediction  # The Agent's output
+    # A system message (or messages) containing the intermediate steps
+    scratchpad: List[BaseMessage]
+    observation: str  # The most recent response from a tool
+
+# %%
+import asyncio
+import platform
+
+
+async def click(state: AgentState):
+    # - Click [Numerical_Label]
+    page = state["page"]
+    click_args = state["prediction"]["args"]
+    if click_args is None or len(click_args) != 1:
+        return f"Failed to click bounding box labeled as number {click_args}"
+    bbox_id = click_args[0]
+    bbox_id = int(bbox_id)
+    try:
+        bbox = state["bboxes"][bbox_id]
+    except Exception:
+        return f"Error: no bbox for : {bbox_id}"
+    x, y = bbox["x"], bbox["y"]
+    await page.mouse.click(x, y)
+    # TODO: In the paper, they automatically parse any downloaded PDFs
+    # We could add something similar here as well and generally
+    # improve response format.
+    return f"Clicked {bbox_id}"
+
+
+async def type_text(state: AgentState):
+    page = state["page"]
+    type_args = state["prediction"]["args"]
+    if type_args is None or len(type_args) != 2:
+        return (
+            f"Failed to type in element from bounding box labeled as number {type_args}"
+        )
+    bbox_id = type_args[0]
+    bbox_id = int(bbox_id)
+    bbox = state["bboxes"][bbox_id]
+    x, y = bbox["x"], bbox["y"]
+    text_content = type_args[1]
+    await page.mouse.click(x, y)
+    # Check if MacOS
+    select_all = "Meta+A" if platform.system() == "Darwin" else "Control+A"
+    await page.keyboard.press(select_all)
+    await page.keyboard.press("Backspace")
+    await page.keyboard.type(text_content)
+    await page.keyboard.press("Enter")
+    return f"Typed {text_content} and submitted"
+
+
+async def scroll(state: AgentState):
+    page = state["page"]
+    scroll_args = state["prediction"]["args"]
+    if scroll_args is None or len(scroll_args) != 2:
+        return "Failed to scroll due to incorrect arguments."
+
+    target, direction = scroll_args
+
+    if target.upper() == "WINDOW":
+        # Not sure the best value for this:
+        scroll_amount = 500
+        scroll_direction = (
+            -scroll_amount if direction.lower() == "up" else scroll_amount
+        )
+        await page.evaluate(f"window.scrollBy(0, {scroll_direction})")
+    else:
+        # Scrolling within a specific element
+        scroll_amount = 200
+        target_id = int(target)
+        bbox = state["bboxes"][target_id]
+        x, y = bbox["x"], bbox["y"]
+        scroll_direction = (
+            -scroll_amount if direction.lower() == "up" else scroll_amount
+        )
+        await page.mouse.move(x, y)
+        await page.mouse.wheel(0, scroll_direction)
+
+    return f"Scrolled {direction} in {'window' if target.upper() == 'WINDOW' else 'element'}"
+
+
+async def wait(state: AgentState):
+    sleep_time = 5
+    await asyncio.sleep(sleep_time)
+    return f"Waited for {sleep_time}s."
+
+
+async def go_back(state: AgentState):
+    page = state["page"]
+    await page.go_back()
+    return f"Navigated back a page to {page.url}."
+
+
+async def to_google(state: AgentState):
+    page = state["page"]
+    await page.goto("https://www.google.com/")
+    return "Navigated to google.com."
+
+# %%
+import base64
+
+from langchain_core.runnables import chain as chain_decorator
+
+# Some javascript we will run on each step
+# to take a screenshot of the page, select the
+# elements to annotate, and add bounding boxes
+with open("mark_page.js") as f:
+    mark_page_script = f.read()
+
+
+@chain_decorator
+async def mark_page(page):
+    await page.evaluate(mark_page_script)
+    for _ in range(10):
+        try:
+            bboxes = await page.evaluate("markPage()")
+            break
+        except Exception:
+            # May be loading...
+            asyncio.sleep(3)
+    screenshot = await page.screenshot()
+    # Ensure the bboxes don't follow us around
+    await page.evaluate("unmarkPage()")
+    return {
+        "img": base64.b64encode(screenshot).decode(),
+        "bboxes": bboxes,
+    }
+
+# %%
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts.loading import load_prompt
+from langchain_openai import ChatOpenAI
+
+
+async def annotate(state):
+    marked_page = await mark_page.with_retry().ainvoke(state["page"])
+    return {**state, **marked_page}
+
+
+def format_descriptions(state):
+    labels = []
+    for i, bbox in enumerate(state["bboxes"]):
+        text = bbox.get("ariaLabel") or ""
+        if not text.strip():
+            text = bbox["text"]
+        el_type = bbox.get("type")
+        labels.append(f'{i} (<{el_type}/>): "{text}"')
+    bbox_descriptions = "\nValid Bounding Boxes:\n" + "\n".join(labels)
+    return {**state, "bbox_descriptions": bbox_descriptions}
+
+
+def parse(text: str) -> dict:
+    action_prefix = "Action: "
+    if not text.strip().split("\n")[-1].startswith(action_prefix):
+        return {"action": "retry", "args": f"Could not parse LLM Output: {text}"}
+    action_block = text.strip().split("\n")[-1]
+
+    action_str = action_block[len(action_prefix) :]
+    split_output = action_str.split(" ", 1)
+    if len(split_output) == 1:
+        action, action_input = split_output[0], None
+    else:
+        action, action_input = split_output
+    action = action.strip()
+    if action_input is not None:
+        action_input = [
+            inp.strip().strip("[]") for inp in action_input.strip().split(";")
+        ]
+    return {"action": action, "args": action_input}
+
+
+# Load prompt
+
+from web_voyager_prompt import prompt;
+
+# %%
+# llm = ChatOpenAI(model="gpt-4-vision-preview", max_tokens=4096)
+llm = ChatOpenAI(model="gpt-4o", max_tokens=16384)
+agent = annotate | RunnablePassthrough.assign(
+    prediction=format_descriptions | prompt | llm | StrOutputParser() | parse
+)
+
+# %%
+import re
+
+def update_scratchpad(state: AgentState):
+    """After a tool is invoked, we want to update
+    the scratchpad so the agent is aware of its previous steps"""
+    old = state.get("scratchpad")
+    if old:
+        txt = old[0].content
+        last_line = txt.rsplit("\n", 1)[-1]
+        step = int(re.match(r"\d+", last_line).group()) + 1
+    else:
+        txt = "Previous action observations:\n"
+        step = 1
+    txt += f"\n{step}. {state['observation']}"
+
+    return {**state, "scratchpad": [SystemMessage(content=txt)]}
+
+# %%
+from langchain_core.runnables import RunnableLambda
+
+from langgraph.graph import END, START, StateGraph
+
+graph_builder = StateGraph(AgentState)
+
+
+graph_builder.add_node("agent", agent)
+graph_builder.add_edge(START, "agent")
+
+graph_builder.add_node("update_scratchpad", update_scratchpad)
+graph_builder.add_edge("update_scratchpad", "agent")
+
+tools = {
+    "Click": click,
+    "Type": type_text,
+    "Scroll": scroll,
+    "Wait": wait,
+    "GoBack": go_back,
+    "Google": to_google,
+}
+
+
+for node_name, tool in tools.items():
+    graph_builder.add_node(
+        node_name,
+        # The lambda ensures the function's string output is mapped to the "observation"
+        # key in the AgentState
+        RunnableLambda(tool) | (lambda observation: {"observation": observation}),
+    )
+    # Always return to the agent (by means of the update-scratchpad node)
+    graph_builder.add_edge(node_name, "update_scratchpad")
+
+
+def select_tool(state: AgentState):
+    # Any time the agent completes, this function
+    # is called to route the output to a tool or
+    # to the end user.
+    action = state["prediction"]["action"]
+    
+    # if action == "ANSWER":  
+    if "ANSWER" in action: # Bug fix: the response is "ANSWER;"
+        return END
+    if action == "retry":
+        return "agent"
+    return action
+
+
+graph_builder.add_conditional_edges("agent", select_tool)
+
+graph = graph_builder.compile()
+
+# %%
+from IPython import display
+from playwright.async_api import async_playwright
+from PIL import Image
+from io import BytesIO
+
+async def call_agent(question: str, page, max_steps: int = 150):
+    event_stream = graph.astream(
+        {
+            "page": page,
+            "input": question,
+            "scratchpad": [],
+        },
+        {
+            "recursion_limit": max_steps,
+        },
+    )
+    final_answer = None
+    
+    img_count = 0
+    steps = []
+    async for event in event_stream:
+        # We'll display an event stream here
+        if "agent" not in event:
+            continue
+        pred = event["agent"].get("prediction") or {}
+        action = pred.get("action")
+        action_input = pred.get("args")
+        display.clear_output(wait=False)
+        steps.append(f"{len(steps) + 1}. {action}: {action_input}")
+        print("\n".join(steps))
+        display.display(display.Image(base64.b64decode(event["agent"]["img"])))
+        
+        img_data = base64.b64decode(event["agent"]["img"])
+        img_buffer = BytesIO(img_data)
+        img = Image.open(img_buffer)
+        img.save(f'imgs/img_{img_count}.jpg')
+        img_count += 1
+
+        if "ANSWER" in action:
+            final_answer = action_input[0]
+            break
+    return final_answer
+
+async def main():
+    print("start")
+    browser = await async_playwright().start()
+    # # We will set headless=False so we can watch the agent navigate the web.
+    browser = await browser.chromium.launch(channel="msedge", headless=False, args=None)
+    # # browser = await playwright.sync_api..chromium.launch(headless=False, args=None)
+    page = await browser.new_page()
+    # _ = await page.goto("https://www.google.com")
+    _ = await page.goto("https://www.google.com/travel/flights")
+
+    # res = await call_agent("Could you explain the WebVoyager paper (on arxiv)?", page)
+    # res = await call_agent("help me check what's the latest nvidia stock price", page)
+    # res = await call_agent("help me get the latest ipad mini deals on dealsea", page)
+    # res = await call_agent("help me check availability of seastar restaurant for tonight for 2 person", page)
+    # res = await call_agent("Find the cost of a 2-year protection for PS4 on Amazon.", page)
+    res = await call_agent("Find the non-stop one-way flight price from SEA to LAX on christmas eve from Delta airline", page)
+    print(f"Final response: {res}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
