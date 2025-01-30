ca7710e1358efc015b20272418aea5cf94d515ce
Ran Wei <ranwei@microsoft.com>
2025-01-30 14:30:05 -0800

    update global

--- src/globals.py
+++ src/globals.py
@@ -5,8 +5,8 @@ OPENAI_EXTRACTION_MODEL = "gpt-4o"  # "gpt-4o-mini"
 OPENAI_REASONING_MODEL = "gpt-4o"
 OPENAI_EXECUTION_MODEL = "gpt-4o"
 
-# USER_QUERY = ""  # Make this empty so it'll ask user to input the query
-USER_QUERY = "book a table at el encannto for 4 people on 2/5 8pm"
+USER_QUERY = ""  # Make this empty so it'll ask user to input the query
+# USER_QUERY = "book a table at el encannto for 4 people on 2/5 8pm"
 #  USER_QUERY = "book a table in wild ginger seattle for 3 people at 7:30pm on 12/24/2024"
 # USER_QUERY = "book a table in el gaucho bellevue on 12/28"
 # USER_QUERY = "book a table in wild ginger seattle for 2 people at 7pm on 12/11"
